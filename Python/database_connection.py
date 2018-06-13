db9f0bb85d25339506bcd3410becbbafc72cf2c4
Navneet Gupta <navneetgupta91@gmail.com>
2018-06-13 13:38:26 +0530

    Update database_connection.py
    This code helps in connecting databases via Python environment.

--- Python/database_connection.py
+++ Python/database_connection.py
@@ -20,7 +20,6 @@ con_2 = mysql.connector.connect(user='your_databse_username', password='your_dat
 df_2 = pd.read_sql_query('SELECT * FROM catalog LIMIT 100;', con_2)
 df_2.head() # Default is 5 rows Use df.head(n='Some Number') to modify
 
-
 con_2.close() # It is a good practice to close the connection post the querying part is completed
 
 
@@ -39,7 +38,7 @@ from sqlalchemy import create_engine
 
 
 ### Making a database connection & fetching data; Change the variables for your own credentails
-# Option 1
+# Option 1 [Using psycopg2 library]
 con = psycopg2.connect(dbname= 'your_database_name', 
                        host='your_database_host_url', 
                        port= 'port_number', user= 'your_database_username', password= 'your_database_password')
@@ -48,7 +47,7 @@ cur_1 = con.cursor()
 cur_1.execute("SELECT * FROM public.catalog LIMIT 100;")
 cur_1.fetchall()
 
-# Option 2
+# Option 2 [Using sqlalchemy library]
 conn = create_engine('postgresql://`username`:`password`@`host_url`:`port_number`/`database`') 
 ## Replace the words inside of backticks with your actual credentails, remember to remove the backticks once done
 
